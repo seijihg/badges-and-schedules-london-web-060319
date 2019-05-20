920f74b67bc50b16963654eed3b6a3af86fc71fa
Le H Ngo <seiji@o2.pl>
2019-05-20 18:09:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(arr)
+  array_names = []
+  
+  arr.each do |name|
+    array_names.push(name)
+  end
+  return array_names
+end
